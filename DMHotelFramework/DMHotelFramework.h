a98b3ba63b8cbbd04c7add3da2422cbeea72abc8
Dario Fanjful <dario.fanjul@despegar.com>
2015-07-22 14:30:16 -0300

    Initial Commit

---
+++ DMHotelFramework/DMHotelFramework.h
@@ -0,0 +1,19 @@
+//
+//  DMHotelFramework.h
+//  DMHotelFramework
+//
+//  Created by Dario Fanjul on 7/22/15.
+//  Copyright (c) 2015 Dario Fanjul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMHotelFramework.
+FOUNDATION_EXPORT double DMHotelFrameworkVersionNumber;
+
+//! Project version string for DMHotelFramework.
+FOUNDATION_EXPORT const unsigned char DMHotelFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMHotelFramework/PublicHeader.h>
+
+
